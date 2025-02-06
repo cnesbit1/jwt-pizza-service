52d2411acc7753d6002d51eadb4a58adb0ede9b6
Cj6701 <127358572+Cj6701@users.noreply.github.com>
2025-02-05 23:38:18 -0700

    Finished franchiseRouter tests

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -23,7 +23,162 @@ beforeAll(async () => {
   });
 
 test('createFranchise with success', async () => {
+    const adminUser = await createAdminUser();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser)
+    adminAuthToken = adminLoginRes.body.token;
 
+    const addRes = await request(app).post("/api/franchise")
+    .set('Content-Type', 'application/json')
+    .set("Authorization", `Bearer ${adminAuthToken}`)
+    .send({"name": randomName(), "admins": [{"email": adminLoginRes.body.user.email}]});
+
+    expect(addRes.status).toBe(200);
 });
 
-  
+test('createFranchise without proper admin permissions', async () => {
+    const adminUser = await createAdminUser();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser)
+    adminAuthToken = adminLoginRes.body.token;
+
+    const addRes = await request(app).post("/api/franchise")
+    .set('Content-Type', 'application/json')
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send({"name": randomName(), "admins": [{"email": adminLoginRes.body.user.email}]});
+
+    expect(addRes.status).toBe(403);
+});
+
+test('deleteFranchise with success', async () => {
+    const adminUser = await createAdminUser();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser)
+    adminAuthToken = adminLoginRes.body.token;
+
+    const addRes = await request(app).post("/api/franchise")
+    .set('Content-Type', 'application/json')
+    .set("Authorization", `Bearer ${adminAuthToken}`)
+    .send({"name": randomName(), "admins": [{"email": adminLoginRes.body.user.email}]});
+    expect(addRes.status).toBe(200);
+
+
+    const delRes = await request(app).delete(`/api/franchise/${addRes.body.id}`)
+        .set("Authorization", `Bearer ${adminAuthToken}`)
+        .send();
+    expect(delRes.status).toBe(200);
+});
+
+test('deleteFranchise without proper admin permissions', async () => {
+    const adminUser = await createAdminUser();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser)
+    adminAuthToken = adminLoginRes.body.token;
+
+    const addRes = await request(app).post("/api/franchise")
+    .set('Content-Type', 'application/json')
+    .set("Authorization", `Bearer ${adminAuthToken}`)
+    .send({"name": randomName(), "admins": [{"email": adminLoginRes.body.user.email}]});
+    expect(addRes.status).toBe(200);
+
+
+    const delRes = await request(app).delete(`/api/franchise/${addRes.body.id}`)
+        .set("Authorization", `Bearer ${testUserAuthToken}`)
+        .send();
+    expect(delRes.status).toBe(403);
+});
+
+test('createStore with success', async () => {
+    const adminUser = await createAdminUser();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser)
+    adminAuthToken = adminLoginRes.body.token;
+
+    const addRes = await request(app).post("/api/franchise")
+    .set('Content-Type', 'application/json')
+    .set("Authorization", `Bearer ${adminAuthToken}`)
+    .send({"name": randomName(), "admins": [{"email": adminLoginRes.body.user.email}]});
+
+    expect(addRes.status).toBe(200);
+    
+    const storeRes = await request(app).post(`/api/franchise/${addRes.body.id}/store`)
+        .set("Content-Type", "application/json")
+        .set("Authorization", `Bearer ${adminAuthToken}`)
+        .send({"franchiseId": addRes.body.id, "name":addRes.body.name});
+
+    expect(storeRes.status).toBe(200);
+});
+
+test('createStore without proper admin permissions', async () => {
+    const adminUser = await createAdminUser();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser)
+    adminAuthToken = adminLoginRes.body.token;
+
+    const addRes = await request(app).post("/api/franchise")
+    .set('Content-Type', 'application/json')
+    .set("Authorization", `Bearer ${adminAuthToken}`)
+    .send({"name": randomName(), "admins": [{"email": adminLoginRes.body.user.email}]});
+
+    expect(addRes.status).toBe(200);
+    
+    const storeRes = await request(app).post(`/api/franchise/${addRes.body.id}/store`)
+        .set("Content-Type", "application/json")
+        .set("Authorization", `Bearer ${testUserAuthToken}`)
+        .send({"franchiseId": addRes.body.id, "name":addRes.body.name});
+
+    expect(storeRes.status).toBe(403);
+});
+
+test('deleteStore with success', async () => {
+    const adminUser = await createAdminUser();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser)
+    adminAuthToken = adminLoginRes.body.token;
+
+    const addRes = await request(app).post("/api/franchise")
+    .set('Content-Type', 'application/json')
+    .set("Authorization", `Bearer ${adminAuthToken}`)
+    .send({"name": randomName(), "admins": [{"email": adminLoginRes.body.user.email}]});
+
+    expect(addRes.status).toBe(200);
+    
+    const storeRes = await request(app).post(`/api/franchise/${addRes.body.id}/store`)
+        .set("Content-Type", "application/json")
+        .set("Authorization", `Bearer ${adminAuthToken}`)
+        .send({"franchiseId": addRes.body.id, "name":addRes.body.name});
+
+    expect(storeRes.status).toBe(200);
+
+    const delRes = await request(app)
+        .delete(`/api/franchise/${addRes.body.id}/store/${storeRes.insertId}`)
+        .set("Authorization", `Bearer ${adminAuthToken}`)
+        .send();
+
+    expect(delRes.status).toBe(200);
+});
+
+test('deleteStore without proper admin permissions', async () => {
+    const adminUser = await createAdminUser();
+    const adminLoginRes = await request(app).put('/api/auth').send(adminUser)
+    adminAuthToken = adminLoginRes.body.token;
+
+    const addRes = await request(app).post("/api/franchise")
+    .set('Content-Type', 'application/json')
+    .set("Authorization", `Bearer ${adminAuthToken}`)
+    .send({"name": randomName(), "admins": [{"email": adminLoginRes.body.user.email}]});
+
+    expect(addRes.status).toBe(200);
+    
+    const storeRes = await request(app).post(`/api/franchise/${addRes.body.id}/store`)
+        .set("Content-Type", "application/json")
+        .set("Authorization", `Bearer ${adminAuthToken}`)
+        .send({"franchiseId": addRes.body.id, "name":addRes.body.name});
+
+    expect(storeRes.status).toBe(200);
+
+    const delRes = await request(app)
+        .delete(`/api/franchise/${addRes.body.id}/store/${storeRes.insertId}`)
+        .set("Authorization", `Bearer ${testUserAuthToken}`)
+        .send();
+
+    expect(delRes.status).toBe(403);
+});
+
+test("getFranchises with success", async()=>{
+    const getRes = await request(app).get("/api/franchise").send();
+    expect(getRes.status).toBe(200);
+  })
